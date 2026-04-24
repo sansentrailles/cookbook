1c6a48739ebb351c6026c201ee701599ea35c4b7
Dmitriy H <you@example.com>
2026-04-25 02:38:30 +0500

    Add models

---
+++ cookbook/models/recipe.py
@@ -0,0 +1,16 @@
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from cookbook.models.base import Base
+
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str] = mapped_column(Text(), nullable=False)
+
+    ingredients: Mapped[list["Ingredients"]] = relationship(
+        secondary="recipe_ingredients", back_populates="recipes"
+    )
