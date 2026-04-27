f9b40d3b267ac9cefdf39b6aa10820f67d872e8c
Dmitriy H <you@example.com>
2026-04-27 13:00:37 +0500

    add repositories

---
+++ cookbook/repositories/ingredient_repository.py
@@ -0,0 +1,34 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from cookbook.models import Ingredient
+
+
+class IngredientRepository:
+    @staticmethod
+    async def get_by_name(db: AsyncSession, name: str):
+        stmt = select(Ingredient).where(Ingredient.name == name)
+        result = await db.execute(stmt)
+        return result.scalar_one_or_none()
+    
+    @staticmethod
+    async def get_by_id(db: AsyncSession, id: int):
+        stmt = select(Ingredient).where(Ingredient.id == id)
+        result = await db.execute(stmt)
+        return result.scalar_one_or_none()
+    
+    @staticmethod
+    async def get_all(db: AsyncSession):
+        stmt = select(Ingredient).order_by(Ingredient.id)
+        result = await db.execute(stmt)
+        return result.scalars().all()
+    
+    @staticmethod
+    async def create(db: AsyncSession, ingredient: Ingredient):
+        db.add(Ingredient)
+        await db.flush()
+        return ingredient
+    
+    @staticmethod
+    async def delet(db: AsyncSession, ingredient: Ingredient):
+        await db.delete(ingredient)
